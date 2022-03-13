0feaaecafbd9a306a66b3059d9dbb0cf75195de2
hannahellis4242 <hannahellis4242@gmail.com>
2022-03-13 22:03:21 +0000

    remove old version

--- tests/romanToNum.test.ts
+++ tests/romanToNum.test.ts
@@ -1,63 +1,64 @@
-type Numeral =
-  | { character: "I"; value: 1 }
-  | { character: "V"; value: 5 }
-  | { character: "X"; value: 10 }
-  | { character: "L"; value: 50 }
-  | { character: "C"; value: 100 }
-  | { character: "D"; value: 500 }
-  | { character: "M"; value: 1000 };
+type Token =
+  | { symbol: "I"; value: 1 }
+  | { symbol: "IV"; value: 4 }
+  | { symbol: "V"; value: 5 }
+  | { symbol: "IX"; value: 9 }
+  | { symbol: "X"; value: 10 }
+  | { symbol: "XL"; value: 40 }
+  | { symbol: "L"; value: 50 }
+  | { symbol: "XC"; value: 90 }
+  | { symbol: "C"; value: 100 }
+  | { symbol: "CD"; value: 400 }
+  | { symbol: "D"; value: 500 }
+  | { symbol: "CM"; value: 900 }
+  | { symbol: "M"; value: 1000 };
 
-const allNumerals: Numeral[] = [
-  { character: "I", value: 1 },
-  { character: "V", value: 5 },
-  { character: "X", value: 10 },
-  { character: "L", value: 50 },
-  { character: "C", value: 100 },
-  { character: "D", value: 500 },
-  { character: "M", value: 1000 },
+const allTokens: Token[] = [
+  { symbol: "I", value: 1 },
+  { symbol: "IV", value: 4 },
+  { symbol: "V", value: 5 },
+  { symbol: "IX", value: 9 },
+  { symbol: "X", value: 10 },
+  { symbol: "XL", value: 40 },
+  { symbol: "L", value: 50 },
+  { symbol: "XC", value: 90 },
+  { symbol: "C", value: 100 },
+  { symbol: "CD", value: 400 },
+  { symbol: "D", value: 500 },
+  { symbol: "CM", value: 900 },
+  { symbol: "M", value: 1000 },
 ];
 
-const toNumeral = (numeralCharacter: string): Numeral | undefined => {
-  return allNumerals.find((n) => n.character === numeralCharacter);
+const allTwoCharTokens = allTokens.filter(({ symbol }) => symbol.length === 2);
+const allSingleCharTokens = allTokens.filter(
+  ({ symbol }) => symbol.length === 1
+);
+
+const parseRomanNumeral = (input: string, total: number): number | null => {
+  if (input.length === 0) {
+    return total;
+  }
+  if (input.length >= 2) {
+    const firstTwoCharacters = input.substring(0, 2);
+    const matchingToken = allTwoCharTokens.find(
+      ({ symbol }) => symbol === firstTwoCharacters
+    );
+    if (matchingToken) {
+      return parseRomanNumeral(input.substring(2), total + matchingToken.value);
+    }
+  }
+  const firstCharacter = input.substring(0, 1);
+  const matchingToken = allSingleCharTokens.find(
+    ({ symbol }) => symbol === firstCharacter
+  );
+  if (matchingToken) {
+    return parseRomanNumeral(input.substring(1), total + matchingToken.value);
+  }
+  return null;
 };
 
-class RomanNumeralResult {
-  constructor(
-    public total: number | null = 0,
-    public previousNumeralValue?: number
-  ) {}
-}
-
-const romanToNum = (romanNumeral: string): number | null => {
-  const result = [...romanNumeral].reduce<RomanNumeralResult>(
-    (result, character) => {
-      if (result.total !== null) {
-        const currentNumeral = toNumeral(character);
-        if (currentNumeral) {
-          if (
-            result.previousNumeralValue &&
-            result.previousNumeralValue < currentNumeral.value
-          ) {
-            //we are in a case where we need to subtract
-            return new RomanNumeralResult(
-              currentNumeral.value -
-                2 * result.previousNumeralValue +
-                result.total,
-              currentNumeral.value
-            );
-          }
-          //otherwise we're in a typical adding situation
-          return new RomanNumeralResult(
-            currentNumeral.value + result.total,
-            currentNumeral.value
-          );
-        }
-      }
-      return new RomanNumeralResult(null);
-    },
-    new RomanNumeralResult()
-  );
-  return result.total;
+const romanToNum2 = (romanNumeral: string): number | null => {
+  return parseRomanNumeral(romanNumeral, 0);
 };
 
 test.each([
@@ -75,6 +76,7 @@ test.each([
   ["M", 1000],
   ["XC", 90],
   ["CM", 900],
+  ["A", null],
 ])("Roman numeral %p = %p", (input: string, expected: number | null) => {
-  expect(romanToNum(input)).toEqual(expected);
+  expect(romanToNum2(input)).toEqual(expected);
 });
